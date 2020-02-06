aef4441ce6ca9ad059848743358d6cc875bffbe1
洪權甫 洪權甫 <60875024H@eduad.ntnu.edu.tw>
2020-02-06 17:48:05 +0800

    add compile C# to dll batch script

---
+++ compile.bat
@@ -0,0 +1,3 @@
+:: You must have visual studio
+
+cmd /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\Roslyn\csc.exe" -target:library -out:CIRLABURControl.dll *.cs
